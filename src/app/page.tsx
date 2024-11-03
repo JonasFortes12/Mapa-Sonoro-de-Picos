fb1f7d84193b73692a08e4c40f4a1e66afa402ac
JonasFortes12 <jonascforte@hotmail.com>
2024-11-03 11:49:45 -0300

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    
+      <h1>Olá Mundo!</h1>
+      <Button>Olá mundo</Button>
+    
+    </>
+  );
+}
